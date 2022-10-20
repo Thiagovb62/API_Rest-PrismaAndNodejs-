d0ea0e6630157e74371d45eb8e456522322a0ba0
Thiago Viana Bezerra <Thiagovb62@gmail.com>
2022-10-20 08:24:11 -0300

    finalizando o crud do usuario com metodo update

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -39,12 +39,73 @@ module.exports = {
         }
 
     },
-    async delete(req, res) {
+    async create(req, res) {
+        try {
 
-        const { id } = req.params
+            const { name, email } = req.body
 
-        await prisma.user.delete({ where: { id: Number(id) } })
+            let user = await prisma.user.findUnique({ where: { email } })
 
-        res.json({ sucess: "deletado com sucesso" })
+            if (user) {
+                return res.json({ error: "ja existe um Usuário com este email" })
+            }
+
+            user = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                },
+            })
+
+            return res.json(user)
+
+        } catch (error) {
+            return res.json({ error })
+        }
+
     },
+    async update(req, res) {
+        try {
+
+            const { name, email } = req.body
+            const { id } = req.params
+
+            let user = await prisma.user.findUnique({ where: { id: Number(id) } })
+
+            if (!user) {
+                return res.json({ error: "user not found" })
+            }
+
+            user = await prisma.user.update({
+                where: { id: Number(id) },
+                data: {
+                    name,
+                    email,
+                }
+            })
+            return res.json({ sucess: "atualizando com sucesso" })
+
+        } catch (error) {
+            return res.json({ error })
+        }
+
+    },
+
+    async delete(req, res) {
+        try {
+
+            const { id } = req.params
+
+            if (!id) {
+                return res.json({ error: "user not found" })
+            }
+
+            await prisma.user.delete({ where: { id: Number(id) } })
+
+            res.json({ sucess: "deletado com sucesso" })
+
+        } catch (error) {
+            return res.json({ error })
+        }
+    }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,5 +7,6 @@ const UserController = require('./controller/UserController')
 routes.get('/user', UserController.index)
 routes.get('/user/:id', UserController.showById)
 routes.post('/user', UserController.create)
+routes.put('/user/:id', UserController.update)
 routes.delete('/user/:id', UserController.delete)
 module.exports = routes;
